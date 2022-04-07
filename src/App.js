3ca1470da082361232d9c7632e8619b3216d2617
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-04-07 17:13:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db } from "firebase";
 function App() {
-  const [posts, setposts] = useState([
-    {
-      Username: "Carry1",
-      caption: " Wow !!! its Working Know ",
-      imageUrl:
-        "https://i.pinimg.com/736x/d2/db/cc/d2dbccd44ca60231089fbb348ddc9a60.jpg",
-    },
-    {
-      Username: "Carry2",
-      caption: " Wow !!! its Working Know ",
-      imageUrl:
-        "https://i.pinimg.com/736x/d2/db/cc/d2dbccd44ca60231089fbb348ddc9a60.jpg",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) =>
+          ({
+            id: doc.id,
+            post: doc.data(),
+          }())
+        )
+      );
+    });
+  }, []);
 
   return (
     <div className="app">
@@ -26,14 +27,15 @@ function App() {
           alt="ErrorImg"
         />
       </div>
-      {posts.map((post) => (
+      {posts.map((id, post) => (
         <Post
+          key={id}
           Username={post.Username}
           caption={post.caption}
           imageUrl={post.imageUrl}
         />
       ))}
-      <Post
+      {/* <Post
         Username="Carry1"
         caption=" Wow !!! its Working Know "
         imageUrl="https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/05/16/906281-carry-minati.jpg"
@@ -42,27 +44,7 @@ function App() {
         Username="Carry2"
         caption=" Wow !!! its Working Know "
         imageUrl="https://i.pinimg.com/736x/d2/db/cc/d2dbccd44ca60231089fbb348ddc9a60.jpg"
-      />
-      <Post
-        Username="Carry3"
-        caption=" Wow !!! its Working Know "
-        imageUrl="https://wallpaperaccess.com/full/2569120.jpg"
-      />
-      <Post
-        Username="Carry4"
-        caption=" Wow !!! its Working Know "
-        imageUrl="https://qph.cf2.quoracdn.net/main-qimg-8c7f3a8a75640acf7c6a69e39ffaee40-lq"
-      />
-      <Post
-        Username="Carry5"
-        caption=" Wow !!! its Working Know "
-        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBGaAJwnApqWS_FMrc-_1TpC_N2apYtRPycg&usqp=CAU"
-      />
-      <Post
-        Username="Carry6"
-        caption=" Wow !!! its Working Know "
-        imageUrl="https://pagalstatus.com/wp-content/webpc-passthru.php?src=https://pagalstatus.com/wp-content/uploads/2021/05/Carry-Minati-Whatsapp-Status-Video.jpg&nocache=1"
-      />
+      /> */}
     </div>
   );
 }
